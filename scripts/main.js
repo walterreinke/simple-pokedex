94a7d1b0dea56b5c021becfb4d0491eedfcf43d9
walterreinke <waltreinke@gmail.com>
2021-04-15 19:29:24 -0400

    filled out the todos in the js

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,10 @@ function setup() {
   It runs attachPokeList when done
 */
 function getPokemonList() {
-  /* TODO 1 */
+  // todo 1
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    .then((response) => response.json())
+    .then((data) => attachPokeList(data.results));
 }
 
 /*
@@ -21,6 +24,9 @@ function getPokemonList() {
 */
 function getPokemon(url) {
   /* TODO 2 */
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => attachPokeView(data));
 }
 
 /*
